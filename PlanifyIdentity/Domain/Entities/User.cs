9951e2c59e3d3f4d77cfc8ee00d80f4b9373f093
EdgarValcarcel <edgar.usa.development@outlook.com>
2024-10-23 23:28:57 -0500

    StatusId deafult = 1;, swagger Documentation, MapGroup for Endpoints, Comments, Password settings.Lockout settings. Removed AddAuthentication()

--- PlanifyIdentity/Domain/Entities/User.cs
+++ PlanifyIdentity/Domain/Entities/User.cs
@@ -5,7 +5,7 @@ namespace PlanifyIdentity.Domain.Entities;
 public class User : IdentityUser
 {
     public bool Locked { get; set; }
-    public int StatusId { get; set; }
+    public int StatusId { get; set; } = 1;
     public virtual Status? Status { get; set; }
     public DateTime PasswordExpDate { get; set; } = DateTime.UtcNow.AddDays(75);
     public DateTimeOffset Created { get; set; }
--- PlanifyIdentity/Program.cs
+++ PlanifyIdentity/Program.cs
@@ -9,17 +9,20 @@ using PlanifyIdentity.Database;
 using PlanifyIdentity.Domain.Entities;
 using PlanifyIdentity.Extensions;
 using PlanifyIdentity.Infrastructure;
+using Swashbuckle.AspNetCore.Filters;
 using Serilog;
 using Serilog.Core;
-using Swashbuckle.AspNetCore.Filters;
+using Microsoft.Extensions.DependencyInjection;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => 
+
+/* Authorization Button on Swagger */
+builder.Services.AddSwaggerGen(options =>
 {
     options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    { 
+    {
         In = ParameterLocation.Header,
         Name = "Authorization",
         Type = SecuritySchemeType.ApiKey
@@ -28,13 +31,13 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
-    .AddCookie(IdentityConstants.ApplicationScheme)
-    .AddBearerToken(IdentityConstants.BearerScheme);
+//builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+//    .AddCookie(IdentityConstants.ApplicationScheme)
+//    .AddBearerToken(IdentityConstants.BearerScheme);*/
 
-builder.Services.AddIdentityCore<User>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddApiEndpoints();
+builder.Services.AddIdentityApiEndpoints<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 /* comment for inmigration */
 builder.Services.AddScoped<ApplicationDbContextInitializer>();
 /* Uncomment for inmigration */
@@ -45,22 +48,21 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequireLowercase = true;
     options.Password.RequireNonAlphanumeric = true;
     options.Password.RequireUppercase = true;
-    options.Password.RequiredLength = 6;
+    options.Password.RequiredLength = 8;
     options.Password.RequiredUniqueChars = 1;
-
     // Lockout settings.
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
     options.Lockout.MaxFailedAccessAttempts = 5;
     options.Lockout.AllowedForNewUsers = true;
-
     // User settings.
     options.User.AllowedUserNameCharacters =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-    options.User.RequireUniqueEmail = false;
+    options.User.RequireUniqueEmail = true;
 });
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+
 //Add support to logging with SERILOG
 builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
@@ -69,13 +71,14 @@ Log.Logger = new LoggerConfiguration()
     .CreateBootstrapLogger();
 
 WebApplication app = builder.Build();
+
 //Initialise and seed database
 using IServiceScope scope = app.Services.CreateScope();
 ApplicationDbContextInitializer initializer = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitializer>();
 if (builder.Configuration.GetValue<bool>("SeedingDatabase"))
 {
     try
-    {
+   {
         await initializer.TrySeedAsync();
     }
     catch (Exception ex)
@@ -91,43 +94,51 @@ if (app.Environment.IsDevelopment())
     app.ApplyMigrations();
 }
 app.UseSerilogRequestLogging();
-app.UseHttpsRedirection();
-app.MapIdentityApi<User>();
 
 app.MapGet("User/me", async (ClaimsPrincipal claims, ApplicationDbContext context) =>
 {
     string userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
 
     return await context.Users.FindAsync(userId);
-}).RequireAuthorization();
-app.MapGet("/", (ClaimsPrincipal user) => $"Hello {user.Identity!.Name}").RequireAuthorization();
+})
+    .WithOpenApi(o => { o.Tags[0].Name = "Users"; return o; })
+    .WithSummary("Data of the user logged in").RequireAuthorization();
+
+app.MapGet("/Hi", (ClaimsPrincipal user) => $"Hello {user.Identity!.Name}")
+    .WithOpenApi(o => { o.Tags[0].Name = "Users"; return o; })
+    .WithSummary("Hi User").RequireAuthorization();
 
 app.MapGet("/Weather", async () =>
-{
-    var client = new HttpClient();
-    client.DefaultRequestHeaders.Accept.Clear();
-    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-    string longitude = "-74.063644";
-    string latitude = "4.624335";
-    string url = $"https://weatherbit-v1-mashape.p.rapidapi.com/current?lon={longitude}&lat={latitude}&units=metric&lang=en";
-    var request = new HttpRequestMessage
     {
-        Method = HttpMethod.Get,
-        RequestUri = new Uri(url),
-        Headers =
-    {
-        { "x-rapidapi-key", "e5d7c26cc2mshfcd70a329e479c7p1b3d94jsnac356a890845" },
-        { "x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com" },
-    },
-    };
-    using HttpResponseMessage response = await client.SendAsync(request);
-    response.EnsureSuccessStatusCode();
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        string longitude = "-74.063644";
+        string latitude = "4.624335";
+        string url = $"https://weatherbit-v1-mashape.p.rapidapi.com/current?lon={longitude}&lat={latitude}&units=metric&lang=en";
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri(url),
+            Headers =
+        {
+            { "x-rapidapi-key", "e5d7c26cc2mshfcd70a329e479c7p1b3d94jsnac356a890845" },
+            { "x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com" },
+        },
+        };
+        using HttpResponseMessage response = await client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
 
-    string responseString = await response.Content.ReadAsStringAsync();
-    WeatherResponse myDeserializedClass = JsonConvert.DeserializeObject<WeatherResponse>(responseString);
+        string responseString = await response.Content.ReadAsStringAsync();
+        WeatherResponse myDeserializedClass = JsonConvert.DeserializeObject<WeatherResponse>(responseString);
 
-    return myDeserializedClass;
-}
-).RequireAuthorization();
+        return myDeserializedClass;
+    }
+    ).WithOpenApi(o => { o.Tags[0].Name = "Weather"; return o; })
+     .WithSummary("City Weather").RequireAuthorization();
+
+app.MapGroup("/Identity")
+    .WithOpenApi(o => { o.Tags[0].Name = "Identity"; return o; })
+    .MapIdentityApi<User>();
 
 await app.RunAsync();
